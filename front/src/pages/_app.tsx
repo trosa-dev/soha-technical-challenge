cd301b8ec3a62031302a85fe9ce1f86798965ae1
Thiago Rosa <thiagosrosa@gmail.com>
2023-12-16 16:36:58 -0300

    chore: add ThemeProvider from MaterialUI

--- front/src/pages/_app.tsx
+++ front/src/pages/_app.tsx
@@ -1,6 +1,25 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "bootstrap/dist/css/bootstrap.css";
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import CssBaseline from "@mui/material/CssBaseline";
+import Head from "next/head";
+import { store } from "@/state/store";
+import { Provider } from "react-redux";
+import { ThemeProvider } from "@emotion/react";
+import { theme } from "@/styles/materialUI";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+	return (
+		<>
+			<Head>
+				<meta name="viewport" content="width=device-width, initial-scale=1" />
+			</Head>
+			<CssBaseline />
+			<Provider store={store}>
+				<ThemeProvider theme={theme}>
+					<Component {...pageProps} />
+				</ThemeProvider>
+			</Provider>
+		</>
+	);
 }
