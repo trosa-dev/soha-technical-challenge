b532744326439ae0f1814fecbdfe3e7551f9d2e2
Thiago Rosa <thiagosrosa@gmail.com>
2023-12-18 01:11:13 -0300

    feat: finished: DashboardPage

--- front/src/pages/dashboard/index.tsx
+++ front/src/pages/dashboard/index.tsx
@@ -1,5 +1,29 @@
 import { DashboardScreen } from "@/screens/dashboard";
+import { RootState } from "@/state/store";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
 
-export default function Home() {
+export default function DashboardPage() {
+	// Initializing the useRouter hook
+	const router = useRouter();
+
+	// Accessing the app state using the useSelector hook
+	const appState = useSelector((state: RootState) => state.app);
+
+	// Extracting the accessToken from the app state
+	const { accessToken } = appState;
+
+	// Using the useEffect hook to perform actions when the component mounts or when the accessToken changes
+	useEffect(() => {
+		// Checking if the accessToken is an empty string
+		if (accessToken === "") {
+			// Redirecting to the home page if the accessToken is empty
+			router.push("/");
+		}
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [accessToken]);
+
+	// Returning the DashboardScreen component if the accessToken is not empty
 	return <DashboardScreen />;
 }
