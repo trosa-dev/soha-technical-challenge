6b16b798b6f1650d788b0817a8d8a13e1d0bd7ea
Thiago Rosa <thiagosrosa@gmail.com>
2023-12-18 01:12:31 -0300

    feat: finished LoginScreen

--- front/src/screens/login/index.tsx
+++ front/src/screens/login/index.tsx
@@ -1,4 +1,4 @@
-import { Button, Tooltip } from "@mui/material";
+import { Button, Tooltip, CircularProgress } from "@mui/material";
 import { LoginRegisterTab } from "./components/LoginRegisterTab";
 import { useSelector } from "react-redux";
 import { RootState } from "@/state/store";
@@ -7,14 +7,19 @@ import { handleOnSubmit } from "./functions/handleOnSubmit";
 import { EmailInput } from "./components/EmailInput";
 import { useEffect, useState } from "react";
 import { PasswordInput } from "./components/PasswordInput";
+import { Errors } from "./components/Errors";
 
+// LoginScreen functional component
 export const LoginScreen = () => {
+	// State to manage the disabled state of the submit button
 	const [disableButton, setDisableButton] = useState(true);
 
+	// Accessing the 'app' state from the Redux store using the useSelector hook
 	const appState = useSelector((state: RootState) => state.app);
-	const { userAction, email, password } = appState;
+	const { userAction, email, password, isLoading } = appState;
 
-	function handleAction(userAction: UserActionEnum) {
+	// Function to determine the action label based on the current user action
+	function handleAction(userAction: UserActionEnum): string {
 		switch (userAction) {
 			case UserActionEnum.login:
 				return "Login";
@@ -25,6 +30,7 @@ export const LoginScreen = () => {
 		}
 	}
 
+	// useEffect to update the disabled state of the submit button based on email and password values
 	useEffect(() => {
 		if (email.value === "" || password.value === "") {
 			setDisableButton(true);
@@ -33,41 +39,57 @@ export const LoginScreen = () => {
 		setDisableButton(false);
 	}, [email.value, password.value]);
 
+	// Rendering the LoginScreen component
 	return (
 		<>
+			{/* Main content container */}
 			<main className="flex items-center justify-center">
 				<div
 					className="m-6 p-6 w-full rounded-lg border-solid border border-soha-color"
 					style={{ maxWidth: 400 }}
 				>
+					{/* LoginRegisterTab component for switching between login and register tabs */}
 					<LoginRegisterTab />
 
+					{/* EmailInput component for handling email input */}
 					<div className="block pt-12">
 						<EmailInput />
 					</div>
 
+					{/* PasswordInput component for handling password input */}
 					<div className="block pt-6 w-full">
 						<PasswordInput />
 					</div>
 
+					{/* Tooltip for displaying a hint when the submit button is disabled */}
 					<Tooltip
 						title={
 							disableButton === true ? "Email and password are required" : ""
 						}
 					>
+						{/* Submit Button */}
 						<div className="block pt-6">
 							<Button
 								disabled={disableButton}
 								variant="contained"
 								fullWidth
 								onClick={() => {
+									// Handling form submission
 									handleOnSubmit();
 								}}
 							>
-								{handleAction(userAction)}
+								{/* Displaying either a loading spinner or the action label */}
+								{isLoading ? (
+									<CircularProgress size={23} color="inherit" />
+								) : (
+									handleAction(userAction)
+								)}
 							</Button>
 						</div>
 					</Tooltip>
+
+					{/* Errors component for displaying error messages */}
+					<Errors />
 				</div>
 			</main>
 		</>
